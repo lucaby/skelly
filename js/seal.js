8f0c0be274d6e5f13a5b1c70075a6dc4eee1562f
unknown <28231539+lucaby@users.noreply.github.com>
2023-03-23 19:58:32 -0300

    add button function

---
+++ js/seal.js
@@ -0,0 +1,5 @@
+function seal_click()
+{
+    document.getElementsByClassName("door_left")[0].className = "door_left_move"
+    document.getElementsByClassName("door_right")[0].className = "door_right_move"
+}
