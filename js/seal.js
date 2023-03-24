1e03a7b8e35391e9a8a191385687770dbd30d30b
unknown <28231539+lucaby@users.noreply.github.com>
2023-03-24 03:29:06 -0300

    improve title fading

--- js/seal.js
+++ js/seal.js
@@ -3,5 +3,5 @@ function seal_click()
     document.getElementsByClassName("door-left")[0].className = "door-left-move";
     document.getElementsByClassName("door-right")[0].className = "door-right-move";
     document.getElementsByClassName("seal-button")[0].className = "seal-button-fade";
-    document.getElementsByClassName("title")[0].className = "title-fade";
+    document.getElementsByClassName("title")[0].classList.add("title-fade");
 }
