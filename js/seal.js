dd08fc25abfae4be49a1345ccf7599bc73fdb383
unknown <28231539+lucaby@users.noreply.github.com>
2023-03-25 01:31:36 -0300

    add fadein effect

--- js/seal.js
+++ js/seal.js
@@ -2,6 +2,6 @@ function seal_click()
 {
     document.getElementsByClassName("door-left")[0].className = "door-left-move";
     document.getElementsByClassName("door-right")[0].className = "door-right-move";
-    document.getElementsByClassName("seal-button")[0].className = "seal-button-fade";
-    document.getElementsByClassName("title")[0].classList.add("title-fade");
+    document.getElementsByClassName("seal-button")[0].className = "seal-button-fadeout";
+    document.getElementsByClassName("title")[0].classList.add("title-fadeout");
 }
