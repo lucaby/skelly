236cef159d8184fd6d23d116d45eaa3885020375
unknown <28231539+lucaby@users.noreply.github.com>
2023-03-25 19:22:30 -0300

    add proper animation

--- js/seal.js
+++ js/seal.js
@@ -4,4 +4,7 @@ function doors_button_click()
     document.getElementsByClassName("door-right")[0].className = "door-right-move";
     document.getElementsByClassName("button-doors")[0].className = "button-doors-fadeout";
     document.getElementsByClassName("title")[0].classList.add("title-fadeout");
+
+    document.getElementsByClassName("title2")[0].classList.add("title2-fadein");
+    document.getElementsByClassName("button-chair")[0].classList.add("button-chair-fadein");
 }
