f2c8d23896520b09cf0d364d4093d02c95b2a507
unknown <28231539+lucaby@users.noreply.github.com>
2023-03-25 16:30:45 -0300

    refactoring

--- js/seal.js
+++ js/seal.js
@@ -1,7 +1,7 @@
-function seal_click()
+function doors_button_click()
 {
     document.getElementsByClassName("door-left")[0].className = "door-left-move";
     document.getElementsByClassName("door-right")[0].className = "door-right-move";
-    document.getElementsByClassName("seal-button")[0].className = "seal-button-fadeout";
+    document.getElementsByClassName("button-doors")[0].className = "button-doors-fadeout";
     document.getElementsByClassName("title")[0].classList.add("title-fadeout");
 }
