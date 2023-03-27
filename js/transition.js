daf74412914e174756acd25641d605905122833c
unknown <28231539+lucaby@users.noreply.github.com>
2023-03-27 16:53:26 -0300

    fix chair button animation

--- js/transition.js
+++ js/transition.js
@@ -5,6 +5,10 @@ function doors_button_click()
     document.getElementsByClassName("button-doors")[0].className = "button-doors-fadeout";
     document.getElementsByClassName("title")[0].classList.add("title-fadeout");
 
+    setTimeout(() => {
+        console.log(document.getElementsByClassName("doors")[0].style.display = "none");
+    }, 1000);
+
     document.getElementsByClassName("title2")[0].classList.add("title2-fadein");
     document.getElementsByClassName("button-chair")[0].classList.add("button-chair-fadein");
 }
