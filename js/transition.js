21c7ef366012c0de0e8ab9cb2e7ef10f71628deb
unknown <28231539+lucaby@users.noreply.github.com>
2023-03-27 21:31:18 -0300

    add transition animation

--- js/transition.js
+++ js/transition.js
@@ -6,9 +6,15 @@ function doors_button_click()
     document.getElementsByClassName("title")[0].classList.add("title-fadeout");
 
     setTimeout(() => {
-        console.log(document.getElementsByClassName("doors")[0].style.display = "none");
+        document.getElementsByClassName("doors")[0].style.display = "none";
     }, 1000);
 
     document.getElementsByClassName("title2")[0].classList.add("title2-fadein");
     document.getElementsByClassName("button-chair")[0].classList.add("button-chair-fadein");
+}
+
+function chair_button_click()
+{
+    document.getElementsByClassName("dark")[0].classList.add("dark-transition");
+    setTimeout(() => {window.location.href = "profile.html";}, 1000);
 }
